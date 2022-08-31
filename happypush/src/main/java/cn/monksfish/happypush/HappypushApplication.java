f34531a8c9c8dd83910aec7aef1fa2072ac0b2be
Cocowwy <514658459@qq.com>
2022-08-31 16:45:53 +0800

    nacos config

--- happypush/src/main/java/cn/monksfish/happypush/HappypushApplication.java
+++ happypush/src/main/java/cn/monksfish/happypush/HappypushApplication.java
@@ -1,13 +1,19 @@
 package cn.monksfish.happypush;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @SpringBootApplication
 public class HappypushApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HappypushApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HappypushApplication.class, args);
+    }
 
 }
