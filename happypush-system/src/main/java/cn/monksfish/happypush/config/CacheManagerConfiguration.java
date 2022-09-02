95264dc468ba8692df5fb166ac0eb2792a657962
Cocowwy <514658459@qq.com>
2022-09-02 10:50:58 +0800

    use redission

--- happypush-system/src/main/java/cn/monksfish/happypush/config/CacheManagerConfiguration.java
+++ happypush-system/src/main/java/cn/monksfish/happypush/config/CacheManagerConfiguration.java
@@ -4,10 +4,10 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 
 /**
@@ -15,7 +15,7 @@ import org.springframework.util.StringUtils;
  * @author cocowwy.cn
  * @create 2022-05-05-11:45
  */
-@Configurable
+@Configuration
 public class CacheManagerConfiguration {
 
     /**
@@ -24,12 +24,13 @@ public class CacheManagerConfiguration {
      * @return
      */
     @Bean("redissonClient")
-    RedissonClient redisson(@Value("${happypush.redis}") String url) {
+    RedissonClient redisson(@Value("${redis.url}") String url,
+                            @Value("${redis.pwd}") String pwd) {
         if (StringUtils.isEmpty(url)) {
             throw new RuntimeException("redis 地址无效");
         }
         Config config = new Config();
-        config.useSingleServer().setAddress(url);
+        config.useSingleServer().setAddress(url).setClientName("root").setPassword(pwd);
         return Redisson.create(config);
     }
 
